fbbc313041fa8409e175f627f87628acc15fa6b6
michaelk0217 <temin.kim@gmail.com>
2024-08-08 14:52:22 -0400

    routers

--- Project3/routers/auth.py
+++ Project3/routers/auth.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
 
-app = FastAPI()
+router = APIRouter()
 
-@app.get("/auth/")
+@router.get("/auth/")
 async def get_user():
     return{'user': 'authenticated'}
